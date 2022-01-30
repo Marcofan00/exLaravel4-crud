4ba1a6f8f0963cf7360e6655eaf59322e4c50098
Marco <marcofan00@gmail.com>
2022-01-30 20:43:16 +0100

    ex done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,10 +1,22 @@
 <?php
 
+
 namespace App\Http\Controllers;
 
+use App\Comic;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function home(){
+        
+        $comics = Comic::all();
+        return view('pages.home', compact('comics'));
+    }
+
+    public function show($id) {
+
+        $comic = Comic:: findOrFail($id);
+        return view('pages.show', compact('comic'));
+    }
 }
